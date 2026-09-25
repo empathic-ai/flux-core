00566d7de92ff8cd27d26516277d31ec3511e6d2
Eden <threeeasywords@gmail.com>
2026-09-25 03:12:18 -0400

    Local models.

--- src/reactive.rs
+++ src/reactive.rs
@@ -1,31 +1,30 @@
 #[cfg(feature = "bevy_reflect")]
-use bevy_reflect::{reflect_trait, DynamicStruct, FromReflect, Reflect, PartialReflect, Typed};
+use bevy_reflect::{DynamicStruct, FromReflect, PartialReflect, Reflect, Typed, reflect_trait};
 
 use ::serde::Serialize;
 
 #[cfg(feature = "bevy_reflect")]
 #[cfg_attr(feature = "bevy", bevy_trait_query::queryable)]
 #[cfg_attr(feature = "bevy", reflect_trait)]
-pub trait Reactive: Reflect {
-
-}
+pub trait Reactive: Reflect {}
 
 #[cfg(feature = "bevy_reflect")]
 pub trait FromDynamic: Typed + FromReflect {
-	fn from_dynamic(value: &DynamicStruct) -> Option<Self> where Self: Sized {
-		if let Some(type_info) = value.get_represented_type_info() {
-			if type_info.type_path() == Self::type_path() {
-				Self::from_reflect(value.as_partial_reflect())
-			} else {
-				None
-			}
-		} else {
-			None
-		}
-	}
+    fn from_dynamic(value: &DynamicStruct) -> Option<Self>
+    where
+        Self: Sized,
+    {
+        if let Some(type_info) = value.get_represented_type_info() {
+            if type_info.type_path() == Self::type_path() {
+                Self::from_reflect(value.as_partial_reflect())
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
 }
 
 #[cfg(feature = "bevy_reflect")]
-impl<T: Typed + FromReflect> FromDynamic for T {
-
-}
+impl<T: Typed + FromReflect> FromDynamic for T {}
